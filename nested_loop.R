cf2dfb46362e81f1c7d567f71fa9601936ed92eb
AbdoulMa <issamadjid1995@gmail.com>
2021-09-24 12:13:31 +0200

    Lines Strips.

--- nested_loop.R
+++ nested_loop.R
@@ -1,2 +1,54 @@
+# Processing P 113 
 library(tidyverse)
 
+lines_stripes <-  tibble(
+  x = seq(20,80,5), 
+  y = seq(20,80,5)
+) %>% 
+  expand(x,y)
+
+lines_stripes <- lines_stripes %>% 
+  mutate(
+    xend = x + 3,
+    yend = ifelse(x %% 10, y = y + 3, y -3)
+  )
+
+lines_stripes %>% 
+  ggplot(aes(x,y , xend = xend, yend = yend)) + 
+  geom_segment(size = 1.5, color = "#111111") + 
+  xlim(c(15,85)) + 
+  ylim(c(15,85)) + 
+  coord_fixed() + 
+  theme_void() + 
+  theme(
+    plot.background = element_rect(fill = "white", color = NA)
+  )
+
+ggsave("Graphics/lines_stripes.png", width = 6, height = 6, device = ragg::agg_png, dpi = 320)
+
+
+# Shifted lines stripes ---------------------------------------------------
+lines_stripes <-  tibble(
+  x = seq(20,85,5), 
+  y = seq(20,85,5)
+) %>% 
+  expand(x,y)
+
+lines_stripes <- lines_stripes %>% 
+  mutate(
+    xend = x - 3,
+    yend = ifelse(x <= 50, y = y - 3, y +3)
+  )
+
+lines_stripes %>% 
+  ggplot(aes(x,y , xend = xend, yend = yend)) + 
+  geom_segment(size = 1.5, color = "#111111") + 
+  xlim(c(15,90)) + 
+  ylim(c(15,90)) + 
+  coord_fixed() + 
+  theme_void() + 
+  theme(
+    plot.background = element_rect(fill = "white", color = NA)
+  )
+
+ggsave("Graphics/lines_stripes_shifted.png", width = 6, height = 6, device = ragg::agg_png, dpi = 320)
