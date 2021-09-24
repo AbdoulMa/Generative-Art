306bedfeab552e7d34409eda6d398480e7a58950
AbdoulMa <issamadjid1995@gmail.com>
2021-09-24 11:40:37 +0200

    Gray squares.

---
+++ gray_squares.R
@@ -0,0 +1,26 @@
+library(tidyverse)
+
+c <- 4 
+ 
+squares_df <- tibble(
+  xmin = 1:4, 
+  ymin = 4:1,
+  fill = c("#ffffff","#cccccc","#999999","#666666")
+) %>% 
+  mutate(
+    xmax = xmin + c, 
+    ymax = ymin + c
+  )
+
+squares_df %>% 
+  ggplot(aes(xmin = xmin, xmax = xmax, ymin= ymin, ymax = ymax)) + 
+  geom_rect(aes(fill = fill), color ="black") + 
+  coord_fixed() + 
+  theme_void() + 
+  scale_fill_identity(guide = "none") + 
+  theme(
+    plot.margin  = margin(30),
+    plot.background = element_rect(fill = "#e6e6e6", color = NA)
+  )
+
+ggsave("Graphics/gray_squares.png", width = 6, height = 6, device = ragg::agg_png, dpi = 320)
