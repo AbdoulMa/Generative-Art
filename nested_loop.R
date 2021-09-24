c5180ff7a4b2365c1d3b4fbd12cb7b843c44cb0c
AbdoulMa <issamadjid1995@gmail.com>
2021-09-24 13:28:54 +0200

    Crossed Lines.

--- nested_loop.R
+++ nested_loop.R
@@ -52,3 +52,33 @@ lines_stripes %>%
   )
 
 ggsave("Graphics/lines_stripes_shifted.png", width = 6, height = 6, device = ragg::agg_png, dpi = 320)
+
+
+# Crossed lines -----------------------------------------------------------
+tibble(
+x = seq(1,100, by = 10),
+y = seq(1,100, by = 10)
+) %>% 
+  expand(x,y) %>% 
+  mutate(
+    x2 = x+6,
+    xend = ifelse(x  < y ,x+6, NA),
+    yend = ifelse(x  < y ,y+6, NA),
+    x2_end = ifelse(x  < y ,x, NA)
+  ) %>% 
+  pivot_longer(cols = c(x,x2), names_to = "nx", values_to = "vx") %>% 
+  pivot_longer(cols = c(xend,x2_end), names_to = "nxend", values_to = "vxend") %>% 
+  select(vx, y, vxend, yend) %>% 
+  drop_na() %>% 
+  filter(vx != vxend) %>% 
+ggplot(aes(vx,y , xend = vxend, yend = yend)) + 
+  geom_segment(size = 1.5, color = "#111111") + 
+  coord_fixed() + 
+  xlim(c(-9,101)) + 
+  ylim(c(-1,107)) + 
+  theme_void()+ 
+  theme(
+    plot.background = element_rect(fill = "white", color = NA)
+  )
+
+ggsave("Graphics/crossed_lines.png", width = 6, height = 6, device = ragg::agg_png, dpi = 320)
